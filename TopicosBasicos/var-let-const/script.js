cc9122c242e1d4b1e3cbbf8ec4f0a843cbb1febf
westorres9 <wester.torres9@gmail.com>
2022-04-02 10:02:34 -0300

    var let and const

---
+++ TopicosBasicos/var-let-const/script.js
@@ -0,0 +1,22 @@
+const x = 10;
+
+if (x > 0) {
+ var a = 100;
+ let b = 200;
+ const c = 300;
+console.log("imprimindo dentro do if");
+console.log(a);
+console.log(b);
+console.log(c); 
+}
+console.log("imprimindo fora do if");
+console.log(a);
+//console.log(b);
+//console.log(c); 
+
+console.log("imprimindo resultado do for");
+for(let i=0; i< 5; i++) {
+  console.log(i);
+}
+
+console.log(i);
