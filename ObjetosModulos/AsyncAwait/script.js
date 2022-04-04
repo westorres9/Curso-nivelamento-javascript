aa0dc82a6eb1abf1fa6c379e13f34ddb9ab2341b
westorres9 <wester.torres9@gmail.com>
2022-04-04 10:48:57 -0300

    Async await

---
+++ ObjetosModulos/AsyncAwait/script.js
@@ -0,0 +1,28 @@
+const cep = "38407270";
+
+const fetchResult = fetch(`https://viacep.com.br/ws/${cep}/json/`);
+
+async function getJsonResponse(url) {
+    const response = await fetch(url);
+    const jsonBody = await response.json();
+    return jsonBody;
+}
+
+getJsonResponse(`https://viacep.com.br/ws/${cep}/json/`)
+    .then(result => {
+        console.log(result);
+    });
+
+async function showCepData(cep) {
+    const url = `https://viacep.com.br/ws/${cep}/json/`;
+    const json = await getJsonResponse(url);
+    console.log(json);
+}
+
+
+showCepData('38407318');
+
+
+
+console.log("A");
+console.log("B");
