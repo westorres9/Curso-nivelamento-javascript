9332cd4fe6407502ef44e7bffe711093d8529079
westorres9 <wester.torres9@gmail.com>
2022-04-04 08:59:40 -0300

    promisses

---
+++ ObjetosModulos/Promisses/script.js
@@ -0,0 +1,28 @@
+function f(g, h) {
+
+    setTimeout(()=> {
+        if(10 < 0) {
+            g("P");
+        }
+        else {
+            h("Q");
+        }
+    }, 3000); 
+}
+
+
+
+
+
+
+const minhaPromise = new Promise(f);
+
+minhaPromise
+    .then(valor => {
+        console.log("Resolveu com o valor " + valor);
+    })
+    .catch(valor => {
+        console.log("Rejeitado com o valor " + valor);
+    });
+
+console.log(minhaPromise);    
